118bc2b49afb8bd2d8111cc24725452c25021b79
aeneasr <aeneas.rekkas@serlo.org>
2017-10-16 20:52:59 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,156 @@
+// @flow
+// createRegistry, createRequestAction, createRequestFailureAction, createRequestSuccessAction, createRootReducer, createRootSaga
+import { createAction, handleActions } from 'redux-actions'
+import { all, call, put, takeLatest } from 'redux-saga/effects'
+
+type Registry = {
+  [key: string]: {
+    fetcher: () => Promise<*>,
+    takeStrategy: Function
+  }
+}
+
+type State = {
+  reduxSagaFetch: {
+    [key: string]: {
+      status: string,
+      payload: mixed
+    }
+  }
+}
+
+export const isFetching = (key: string) => (state: State) => state.reduxSagaFetch[key].status === STATE_FETCHING
+export const isFetchFailure = (key: string) => (state: State) => state.reduxSagaFetch[key].status === STATE_FAILURE
+export const isFetchSuccess = (key: string) => (state: State) => state.reduxSagaFetch[key].status === STATE_SUCCESS
+export const selectPayload = (key: string) => (state: State) => state.reduxSagaFetch[key].payload
+
+const createDefaultWorker = (fetcher, successAction, failureAction) => function* () {
+  try {
+    const result = yield call(fetcher)
+    yield put(successAction(result))
+  } catch (error) {
+    yield put(failureAction(error))
+  }
+}
+
+const createWatcher = (action, worker, takeStrategy) => function* () {
+  yield takeStrategy(action, worker)
+}
+
+const createWatchers = (registry: Registry) => Object.keys(registry).map((key) => {
+  const { fetcher, takeStrategy } = registry[key]
+  const action = createRequestAction(key)
+  const worker = createDefaultWorker(
+    fetcher,
+    createRequestSuccessAction(key),
+    createRequestFailureAction(key),
+  )
+
+  return createWatcher(action, worker, takeStrategy)()
+})
+
+const STATE_FETCHING = 'FETCHING'
+const STATE_SUCCESS = 'SUCCESS'
+const STATE_FAILURE = 'FAILURE'
+
+class SagaFetcher {
+  registry: Registry = {}
+
+  constructor(config: {
+    [key: string]: {
+      fetcher: () => Promise<*>,
+      takeStrategy: any
+    }
+  } = {}) {
+    if (typeof config !== 'object') {
+      throw new Error(`Registry must be an object but got ${typeof config}`)
+    }
+
+    Object.keys(config).forEach(key => {
+      const options = config[key]
+      this.add(key, options.fetcher, options.takeStrategy)
+    })
+  }
+
+  add = (key: string, fetcher: (arg: any) => Promise<*>, takeStrategy: Function = takeLatest) => {
+    if (typeof fetcher !== 'function') {
+      throw new Error(`Expected a function for key ${key} but got ${typeof fetcher}`)
+    }
+
+    this.registry[key] = {
+      fetcher, takeStrategy: takeStrategy
+    }
+  }
+
+  wrapRootReducer = (reducers: any = {}) => {
+    const handlers = {}
+
+    Object.keys(this.registry).forEach((key) => {
+      handlers[createRequestAction(key).toString()] = (state, action) => ({
+        ...state,
+        [key]: { status: STATE_FETCHING }
+      })
+      handlers[createRequestSuccessAction(key).toString()] = (state, { payload }) => ({
+        ...state,
+        [key]: { status: STATE_SUCCESS, payload }
+      })
+      handlers[createRequestFailureAction(key).toString()] = (state, { payload }) => ({
+        ...state,
+        [key]: { status: STATE_FAILURE, payload }
+      })
+    })
+
+    return ({
+      ...reducers,
+      reduxSagaFetch: handleActions(handlers, {})
+    })
+  }
+
+  createRootSaga = () => {
+    const { registry } = this
+    return function* rootSaga(): Generator<*, *, *> {
+      yield all(createWatchers(registry))
+    }
+  }
+}
+
+/**
+ * Creates a new redux-saga-fetch registry.
+ *
+ *   const registry = createSagaFetcher({
+ *     user: {
+ *       fetcher: (id) => superagent.get('http://myapi.com/users/' + id)
+ *     }
+ *   })
+ *
+ * @param config
+ */
+export const createSagaFetcher = (config: Registry) => new SagaFetcher(config)
+
+/**
+ * Create a request action creator for redux.
+ *
+ * @param key
+ */
+export const createRequestAction = (key: string) =>
+  createAction(`REDUX_SAGA_FETCH_${key.toUpperCase()}_REQUEST`)
+
+/**
+ * Creates an action that is dispatched once a request action succeeds. This is an internal method and not part
+ * of the public api.
+ *
+ * @private
+ * @param key
+ */
+export const createRequestSuccessAction = (key: string) =>
+  createAction(`REDUX_SAGA_FETCH_${key.toUpperCase()}_SUCCESS`)
+
+/**
+ * Creates an action that is dispatched once a request action fails. This is an internal method and not part
+ * of the public api.
+ *
+ * @private
+ * @param key
+ */
+export const createRequestFailureAction = (key: string) =>
+  createAction(`REDUX_SAGA_FETCH_${key.toUpperCase()}_FAILURE`)
